6c6ea1b203deaae50dfaa9f5d87f67872a4c97b9
Love <lovesoni607@gmail.com>
2022-02-23 22:30:22 +0530

    script to launch browser

---
+++ src/test/java/LaunchBrowser.java
@@ -0,0 +1,21 @@
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+import java.nio.file.Paths;
+
+public class LaunchBrowser {
+    public static void main(String[] args) {
+       Playwright playwright =  Playwright.create();
+      Browser browser =  playwright.webkit().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        Page page =browser.newPage();
+        page.navigate("https://www.google.com");
+        System.out.println(page.title());
+        page.type("input[name='q']","love soni");
+        page.click("//input[@name='btnK']");
+        page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("example.png")));
+        browser.close();
+        playwright.close();
+    }
+}
